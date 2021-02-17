e089787030cb035e866e468ca20d3aa2bbee28fe
Reuben Raff <reubenraff@Reubens-MacBook-Pro.local>
2021-02-17 13:02:52 -0500

    added new condition to is_even.py file

--- is_even.py
+++ is_even.py
@@ -1,7 +1,10 @@
 val = input("Enter a num: ")
 
 # Nice!
+# pretty conditions!
 if (int(val) % 2) == 0:    
     print("You've entered an even number!")
+elif (int(val) % 7) == 0:
+    print("You might've entered a prime!")
 else:
     print("You've got an odd number!")
